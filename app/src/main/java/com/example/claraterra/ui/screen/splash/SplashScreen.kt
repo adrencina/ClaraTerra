c58e353a7c07eefb83459552d78466b60faeb21c
claudio.castillo <claudioch.talentoenti@gmail.com>
2025-06-11 22:31:17 -0600

    Se agrego imagen y texto a pantalla Splash para SplasScreen

--- app/src/main/java/com/example/claraterra/ui/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/example/claraterra/ui/screen/splash/SplashScreen.kt
@@ -1,13 +1,22 @@
 package com.example.claraterra.ui.screen.splash
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import kotlinx.coroutines.delay
+import com.example.claraterra.R
+
+val rochester = FontFamily(Font(R.font.rochester_regular))
 
 @Composable
 fun SplashScreen(navController: NavController) {
@@ -32,9 +41,14 @@ fun SplashScreen(navController: NavController) {
         contentAlignment = Alignment.Center
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            CircularProgressIndicator()
+            Image(painter = painterResource(id = R.drawable.iconoclaraterra), contentDescription = "Imagen Clara Terra")
+            Text(
+                text = "Clara Terra",
+                style = TextStyle(fontFamily = rochester),
+                fontSize = 44.sp
+            )
+            //CircularProgressIndicator()
             Spacer(modifier = Modifier.height(16.dp))
-            Text("Cargando...")
         }
     }
 }
